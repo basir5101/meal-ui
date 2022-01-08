3646e6bb122029d256881f1afa1ec9c034c25795
Abdul Basir <basir.bsmrstu@gmail.com>
2022-01-08 22:48:56 +0600

    design

--- pages/meals/update-shopping/[id].jsx
+++ pages/meals/update-shopping/[id].jsx
@@ -64,11 +64,10 @@ class Shopping extends Component {
     return (
       <CommonLayout className="md:container md:mx-auto mt-5">
         <div className="w-full">
-          <div className="bg-gradient-to-b from-blue-800 to-blue-600 h-96"></div>
           <div className="max-w-5xl mx-auto px-6 sm:px-6 lg:px-8 mb-12">
-            <div className="bg-white w-full shadow rounded p-8 sm:p-12 -mt-72">
+            <div className="bg-white w-full shadow rounded p-2 sm:p-12">
               <p className="text-3xl font-bold text-indigo-600 leading-7 text-center">
-                {this.props.meal.title}{" "}
+                {this.props.meal.title}
                 <span className="text-blue-600">- Update Shopping</span>
               </p>
               <form onSubmit={(e) => e.preventDefault()}>
